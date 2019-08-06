64f83927966c618157638de49cd6b18ca1f9afeb
System Administrator <root@Joeys-MacBook-Pro.local>
2019-08-06 14:18:35 -0500

    Switch SSL ports to 443:core, 8443:minio

--- full_install.sh
+++ full_install.sh
@@ -3,7 +3,16 @@ if [ "$(id -u)" != "0" ]; then
 exec sudo "$0" "$@"
 fi
 cd /opt/
+#Check OS to use appropriate package manager & turn off selinux for Centos 7
+if [ -f /etc/redhat-release ]; then
+  OS_CHECK=yumpm
+  sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+  set enforce 0 sestatus
+fi
 
+if [ -f /etc/lsb-release ]; then
+  OS_CHECK=aptgetpm
+fi
 #Prompt for SSL/TLS support
 read -p "Install SSL/TLS Certificates (y/n)? " tls_answer
   case ${tls_answer:0:1} in
@@ -47,22 +56,59 @@ case ${ec2_answer:0:1} in
     ;;
 esac
 #Update packages
-sudo apt-get update -y
-#Install expect
-sudo apt-get install -y expect
-################################################################################################
-#Install Docker
-sudo apt-get -y install docker.io
-#Create links
-sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+if [ "$OS_CHECK" = "aptgetpm" ]; then
+    sudo apt-get update -y
+    #Install expect
+    sudo apt-get install -y expect
+    ################################################################################################
+    #Install Docker
+    sudo apt-get -y install docker.io
+    #Create links
+    sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 
-################################################################################################
-#Install PostgreSQL
-sudo apt-get -y install postgresql postgresql-contrib
+    ################################################################################################
+    #Install PostgreSQL
+    sudo apt-get -y install postgresql postgresql-contrib
+fi
+if [ "$OS_CHECK" = "yumpm" ]; then
+    sudo yum update -y
+    #Install expect
+    sudo yum install -y expect
+    #Install XXD which is used in the configure.sh
+    sudo yum install -y vim-common
+    ################################################################################################
+    #Install Docker
+    sudo yum install -y docker
+    #Start Docker
+    systemctl start docker
+    #Create links
+    #sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 
+    ################################################################################################
+    #Install PostgreSQL
+    sudo yum install -y postgresql-server postgresql-contrib
+    sudo postgresql-setup initdb
+    sudo systemctl start postgresql
+    sudo systemctl enable postgresql
+    ###############################################################################################
+    ####download setup.sh for Centos support
+    sudo mv /opt/fossa/setup.sh /opt/fossa/setup.sh.bak
+    yum install -y wget
+    wget -c https://raw.githubusercontent.com/joey-fossa/fossa-install/master/setup_centos.sh -O /opt/fossa/setup.sh
+    sudo chmod +x /opt/fossa/setup.sh
+fi
 ################################################################################################
 # Download and run the installer
 mkdir -p /opt/fossa && curl -L https://github.com/fossas/fossa-installer/archive/v0.0.21.tar.gz | tar -zxv -C /opt/fossa --strip-components=1 && chmod a+x /opt/fossa/boot.sh && sudo ln -sf /opt/fossa/boot.sh /usr/local/bin/fossa && cd /opt/fossa
+export PATH=$PATH:/usr/local/bin
+if [ "$OS_CHECK" = "yumpm" ]; then
+    ####download setup.sh for Centos support
+    sudo mv /opt/fossa/setup.sh /opt/fossa/setup.sh.bak
+    yum install -y wget
+    sudo wget -c https://raw.githubusercontent.com/joey-fossa/fossa-install/master/setup_centos.sh -O /opt/fossa/setup.sh
+    sudo chmod +x /opt/fossa/setup.sh
+fi
+
 # Setup fossa
 sudo ./setup.sh
 # Add user to docker group
@@ -74,7 +120,7 @@ set force_conservative 0
 set send_slow {1 .1}                         
 #
 set timeout -1
-spawn sudo fossa init
+spawn fossa init
 match_max 100000
 expect -exact "Initializing Fossa\r
 Please provide docker login credentials.\r
@@ -110,7 +156,7 @@ echo "Add Minio buckets"
   mc mb minio1/archive.fossa.io && \  
   exit" 
 
-#If TLS/SSL is in play, commit the minio image configuration, stop and remove the container and start a new container with port 443
+#If TLS/SSL is in play, commit the minio image configuration, stop and remove the container and start a new container with port 8443
 #Also copy the existing certificates to the minio required folder /mnt/config/certs with the required names of private.key and public.crt
   case ${tls_answer:0:1} in
     y|Y )
@@ -123,7 +169,7 @@ echo "Add Minio buckets"
         cp $file_dir/$key /mnt/config/certs/private.key
         cp $file_dir/$cert /mnt/config/certs/public.crt
         #Restart minio with SSL port
-        /usr/bin/docker run -p 443:9000 --name minio1 -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data &  
+        /usr/bin/docker run -p 8443:9000 --name minio1 -e "MINIO_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE" -e "MINIO_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" -v /mnt/data:/data -v /mnt/config:/root/.minio minio/minio server /data &  
         sleep 10s
     ;;
     * )
@@ -165,14 +211,14 @@ sudo echo "componentUploader__bucket=archive.fossa.io"  >> /opt/fossa/config.env
 
 case ${tls_answer:0:1} in
     y|Y )
-        #If SSL in play set minio endpoint ports to 443
-        sudo echo -E "cache__package__s3Options__endpoint="$PRIMARY_IP":443"  >> /opt/fossa/config.env
-        sudo echo -E "s3__endpoint="$PRIMARY_IP":443"  >> /opt/fossa/config.env
+        #If SSL in play set minio endpoint ports to 8443
+        sudo echo -E "cache__package__s3Options__endpoint=https://"$PRIMARY_IP":8443"  >> /opt/fossa/config.env
+        sudo echo -E "s3__endpoint=https://"$PRIMARY_IP":8443"  >> /opt/fossa/config.env
     ;;
     * )
         #If no SSL set minio endpoint to standard 9000
-        sudo echo -E "cache__package__s3Options__endpoint="$PRIMARY_IP":9000"  >> /opt/fossa/config.env
-        sudo echo -E "s3__endpoint="$PRIMARY_IP":9000"  >> /opt/fossa/config.env
+        sudo echo -E "cache__package__s3Options__endpoint=http://"$PRIMARY_IP":9000"  >> /opt/fossa/config.env
+        sudo echo -E "s3__endpoint=http://"$PRIMARY_IP":9000"  >> /opt/fossa/config.env
     ;;
 esac
 ################################################################################################
@@ -197,7 +243,7 @@ case ${tls_answer:0:1} in
         sed -i "s/$SED_SRC_TXT/&"' -v $CERTDIR:\/fossa\/certs'"/" /opt/fossa/boot.sh
 
         #Change SSL port to 8443
-        sed -i 's/443:443/8443:8443/g' /opt/fossa/boot.sh
+        #sed -i 's/443:443/8443:8443/g' /opt/fossa/boot.sh
 
         #Backup config.env
         sudo cp /opt/fossa/config.env /opt/fossa/config.env.bak
@@ -212,8 +258,7 @@ case ${tls_answer:0:1} in
     ;;
 esac
 ################################################################################################
-sudo service postgresql stop
-
+sudo systemctl stop postgresql
 ################################################################################################
 #(cd /opt/fossa;sudo fossa start 1) > /dev/null 2>&1 & disown
 ################################################################################################
